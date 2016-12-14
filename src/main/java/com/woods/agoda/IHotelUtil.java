4e40307752a23ef53548d578f1fedecce9231266
John <jowoods@darksideproductions.net>
2016-12-14 11:26:59 -0800

    add exception class

--- src/main/java/com/woods/agoda/IHotelUtil.java
+++ src/main/java/com/woods/agoda/IHotelUtil.java
@@ -3,6 +3,6 @@ package com.woods.agoda;
 import com.woods.agoda.data.Hotel;
 import java.util.List;
 
-public class IHotelUtil{
+public interface IHotelUtil{
 	public List<Hotel> retrieveHotels(String path, String cityFilter ,String sortBy, String ascending);
 } 
